add84447f3e9480a7ad92594be21c38c89cd9812
Andrew Nichols <tandrewnichols@gmail.com>
2017-12-04 14:29:01 -0500

    Timeout just to see if I can make something work

--- test/gulp-remember-cache.js
+++ test/gulp-remember-cache.js
@@ -105,14 +105,10 @@ describe('gulp-remember-cache', () => {
             }));
         };
         run(2);
-        console.log(process.cwd());
-        let watcher = gulp.watch(`${__dirname}/fixtures/**/*`, { delay: 0 }, (event) => {
-          console.log('closing watcher');
-          watcher.close();
-          return run(1).pipe(assert.end(done));
-        });
         touch(`${__dirname}/fixtures/apple.js`, () => {
-          console.log('touch called');
+          setTimeout(() => {
+            run(1).pipe(assert.end(done));
+          }, 1000);
         });
       });
 
