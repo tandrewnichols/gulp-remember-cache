6270d594c27f57997868a12d4479ac035d1ad7f8
Andrew Nichols <tandrewnichols@gmail.com>
2018-01-05 22:20:47 -0500

    Use my version of gulp-codeclimate-reporter

--- gulp/codeclimate.js
+++ gulp/codeclimate.js
@@ -5,8 +5,10 @@ gulp.task('codeclimate', function() {
   if (process.version.indexOf('v8') > -1) {
     return gulp.src('coverage/lcov.info', { read: false })
       .pipe(codeclimate({
-        token: 'e7d897b341e458ed1c6fde47d4c1f314a237d9f83d7fa990dff7ba1c14147cea'
+        token: process.env.CODECLIMATE_REPO_TOKEN
       }));
+  } else {
+    done();
   }
 });
 
